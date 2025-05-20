89d142e3f375fbe0c70ef6fa8b4bc7209cc4d9dd
ciriwitcher <74843116+ciriwitcher@users.noreply.github.com>
2025-05-21 01:08:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -72,5 +72,5 @@ app.post("/verify", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`🚀 Server is running on port ${PORT}`));
